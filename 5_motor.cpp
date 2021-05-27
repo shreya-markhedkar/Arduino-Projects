164391ac63f6a62d009f39725098e8836fa33469
Shreya Markhedkar <81920073+shreya-markhedkar@users.noreply.github.com>
2021-05-27 23:14:15 +0530

    Create 5_motor.cpp

---
+++ 5_motor.cpp
@@ -0,0 +1,26 @@
+// C++ code
+//
+int motorPin=9;
+
+void setup()
+{
+  pinMode(motorPin,OUTPUT);
+}
+void loop()
+{
+  for (int i=0; i<255;i++)
+  {
+    analogWrite(motorPin,i);
+    delay(10);
+  }
+  
+  delay(500);
+  
+  for(int i=255;i>=0;i--)
+  {
+    analogWrite(motorPin,i);
+    delay(10);
+  }
+  
+  delay(500);
+}
